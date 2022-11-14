9be3353534087ba16a92e419523ac02ab8b205d8
anesainzt <anesainzt@gmail.com>
2022-11-14 02:24:08 +0100

    Modelo completo

--- Proyecto-IW/myapp/views.py
+++ Proyecto-IW/myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404, get_list_or_404
-from django.shortcuts import render
-from .models import Genero, Artistas, Grupos, Disco
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Artistas, Disco, Genero, Grupos
+
 
 def index(request):
     generos = Genero.objects.order_by('nomGenero')
@@ -21,3 +22,44 @@ def lista_discos(request):
     disco = Disco.objects.order_by('nomDisco')
     context = {'lista_discos': disco}
     return render(request, 'lista_discos.html', context)
+
+#devuelve los artistas de un genero o discos
+def artistasG(request, genero_id):
+	genero = get_object_or_404(Genero, pk=genero_id)
+	artistas =  genero.artistas_set.all()
+	context = {'genero': genero, 'artistas' : artistas }
+	return render(request, 'artistas.html', context)
+
+def artistasD(request, disco_id):
+	discos = get_object_or_404(Disco, pk=disco_id)
+	artistas =  discos.artistas_set.all()
+	context = { 'artistas' : artistas, 'discos': discos }
+	return render(request, 'artistas.html', context)
+
+#Devuelve los grupos de un genero o disco
+def grupoG(request, genero_id):
+	genero = get_object_or_404(Genero, pk=genero_id)
+	grupo =  genero.grupo_set.all()
+	context = {'genero': genero, 'grupo' : grupo }
+	return render(request, 'grupos.html', context)
+
+def grupoD(request, disco_id):
+	discos = get_object_or_404(Disco, pk=disco_id)
+	grupo =  discos.grupo_set.all()
+	context = { 'grupo' : grupo, 'discos': discos }
+	return render(request, 'grupos.html', context)
+
+#devuelve los detalles de un artista y grupo
+def artista(request, artista_id):
+    artista = get_object_or_404(Artistas, pk=artista_id)
+    genero =  artista.genero.all()
+    discos =  artista.discos.all()
+    context = { 'artista': artista, 'genero' : genero, 'discos' : discos }
+    return render(request, 'artistas.html', context)
+
+def grupo(request, grupo_id):
+    grupo = get_object_or_404(Grupos, pk=grupo_id)
+    genero =  grupo.genero.all()
+    discos =  grupo.discos.all()
+    context = { 'grupo': grupo, 'genero' : genero, 'discos' : discos }
+    return render(request, 'grupos.html', context)
--- Proyecto-IW/mysite/urls.py
+++ Proyecto-IW/mysite/urls.py
@@ -23,7 +23,13 @@ urlpatterns = [
     path('lista_artistas', views.lista_artistas, name='lista_artistas'),
     path('lista_grupos', views.lista_grupos, name='lista_grupos'),
     path('lista_discos', views.lista_discos, name='lista_discos'),
-    
+    path('genero/<int:genero_id>/artistas', views.artistasG, name='artistas'),
+    path('discos/<int:disco_id>/artistas', views.artistasD, name='artistas'),
+    path('genero/<int:genero_id>/grupo', views.grupoG, name='grupo'),
+    path('discos/<int:disco_id>/grupo', views.grupoD, name='grupo'),
+    path('artista/<int:artista_id>', views.artista, name='artista'),
+    path('grupo/<int:grupo_id>', views.grupo, name='grupo'),
+
     path('myapp/', include('myapp.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index')
